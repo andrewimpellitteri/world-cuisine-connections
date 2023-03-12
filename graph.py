67bc1ca7517a5c6234b7dc2d2bd592a408568bb5
andrewimpellitteri <aimpellitteri1@gmail.com>
2023-03-12 17:34:14 -0400

    update

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('look.csv')
 new_df = pd.DataFrame(index=df.columns, columns=df.columns)
@@ -19,16 +20,17 @@ for col1 in df.columns:
 new_df = new_df[new_df.columns[1:]].fillna(0)
 new_df = new_df.iloc[1:, :]
 new_df = (new_df - new_df.min()) / (new_df.max() - new_df.min())
-print(new_df)
 
-
+new_df = new_df.replace(0, np.nan)
+for col in new_df.columns:
+    print(col)
+    print(new_df.nsmallest(5, col))
+"""
 G = nx.from_numpy_array(new_df.values)
 G = nx.relabel_nodes(G, dict(enumerate(df.columns)))
 
 edges = G.edges()
-weights = [G[u][v]['weight'] ** 4 for u,v in edges]
-print(weights)
-
+weights = [ 1.1 * G[u][v]['weight'] for u,v in edges]
 
 plt.figure(figsize=(16,9))
 
@@ -37,3 +39,5 @@ nodelist = df.columns
 nx.draw(G, nodelist=nodelist, with_labels=True, width=weights)
 
 plt.show()
+"""
+
